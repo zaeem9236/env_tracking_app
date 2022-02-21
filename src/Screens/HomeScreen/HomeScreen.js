7fa404ae4341c7a63a899126a877443ec229d0c4
Zaeem Ahmad <60064010+zaeem9236@users.noreply.github.com>
2022-02-21 21:31:23 +0500

    top dashboard created

--- src/Screens/HomeScreen/HomeScreen.js
+++ src/Screens/HomeScreen/HomeScreen.js
@@ -1,38 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { ButtonGroup } from 'react-native-elements'
 import { useSelector, useDispatch } from 'react-redux';
 import { countUp, countDown, getCount } from '../../Redux/Slices/CountDownSlice';
 
 import { logOut } from '../../Firebase/functions/logOut';
-import { firebaseDataPush } from '../../Firebase/functions/firebaseDataPush';
-
+import { Dimensions } from 'react-native';
+const windowHeight = Dimensions.get('window').height;
 
 const HomeScreen = ({ navigation }) => {
-  let count = useSelector(getCount);
+  let [selectedIndex, setSelectedIndex] = useState(0)
   let dispatch = useDispatch();
   return (
-    <View style={styles.mainContainer}>
-      <View style={styles.section1}>
-        <TouchableOpacity style={styles.counterButton} onPress={() => { dispatch(countUp()) }}>
-          <Text style={styles.buttonText}>Up</Text>
-        </TouchableOpacity>
+    <View style={styles.mainContainer(windowHeight)}>
+      <View style={styles.topDashboardContainer}>
+        <View style={styles.topRow}>
+          <Text style={styles.topRow_heading}>{`Welcome user`}</Text>
+          <TouchableOpacity style={styles.topRow_button}>
+            <Text style={styles.topRow_buttonText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.screenSelectRow}>
+          <ButtonGroup
+            buttons={['Status', 'Update Number']}
+            selectedIndex={selectedIndex}
+            onPress={(value) => {
+              setSelectedIndex(value);
+            }}
+            containerStyle={{ marginBottom: 0 }}
+            selectedButtonStyle={{ backgroundColor: 'rgb(1, 87, 155)' }}
+          />
+        </View>
 
-        <Text style={{ fontSize: 40 }}>{count}</Text>
+      </View>
 
-        <TouchableOpacity style={styles.counterButton} onPress={() => { dispatch(countDown()) }}>
-          <Text style={styles.buttonText}>Down</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.section2}>
-        <TouchableOpacity style={styles.button} onPress={() => { firebaseDataPush() }}>
-          <Text style={styles.buttonText}>Firebase data push</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.section2}>
-        <TouchableOpacity style={styles.button} onPress={() => { logOut(navigation) }}>
-          <Text style={styles.buttonText}>Sign out</Text>
-        </TouchableOpacity>
-      </View>
+      <Text>sdfdsfdsfds</Text>
+
     </View>
   )
 }
@@ -40,37 +43,38 @@ const HomeScreen = ({ navigation }) => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
+  mainContainer: function (windowHeight) {
+    return {
+      height: windowHeight
+    }
   },
-  section1: {
-    flex: 0.4,
-    justifyContent: 'center',
-    alignItems: 'center'
+  topDashboardContainer: {
+    // backgroundColor: 'red',
+    flex: 0.2,
   },
-  section2: {
-    flex: 0.4,
-    justifyContent: 'center',
-    alignItems: 'center'
+  topRow: {
+    // backgroundColor: 'green',
+    flexDirection: 'row',
+    paddingHorizontal: '5%',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    flex: 0.5,
   },
-  button: {
-    width: '85%',
-    height: '20%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 8,
-    backgroundColor: 'rgba(0,0,0,0.3)',
+  topRow_heading: {
+    fontSize: 20,
+    color: 'rgb(1, 87, 155)'
   },
-  counterButton: {
-    width: '85%',
-    height: '20%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 8,
-    backgroundColor: 'rgba(1,57,150,0.3)',
+  topRow_button: {
+
   },
-  buttonText: {
-    color: 'white',
-    fontSize: 22
+  topRow_buttonText: {
+    fontSize: 18,
+    color: 'rgba(255, 0, 0, 0.7)'
+  },
+  screenSelectRow: {
+    // backgroundColor: 'yellow',
+    flex: 0.5,
+    justifyContent: 'flex-end',
   }
+
 })
