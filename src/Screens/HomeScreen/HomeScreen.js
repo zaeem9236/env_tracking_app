5fc844ceb8e53d5d4893c83f48079a109ee5f979
Zaeem Ahmad <60064010+zaeem9236@users.noreply.github.com>
2022-02-23 22:49:10 +0500

    notification added

--- src/Screens/HomeScreen/HomeScreen.js
+++ src/Screens/HomeScreen/HomeScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import { ButtonGroup } from 'react-native-elements'
 import { useSelector, useDispatch } from 'react-redux';
 import { getUserDetails } from '../../Redux/Slices/userDetailsSlice';
+import { getDeviceData as iotData } from '../../Redux/Slices/deviceDataSlice';
 import { updateDeviceData } from '../../Redux/Slices/deviceDataSlice';
 import StatusScreen from '../StatusScreen/StatusScreen';
 import UpdateNumberScreen from '../UpdateNumberScreen/UpdateNumberScreen';
@@ -10,6 +11,7 @@ import MapScreen from '../MapScreen/MapScreen';
 import { getDeviceData } from '../../Firebase/functions/getDeviceData';
 import { logOut } from '../../Firebase/functions/logOut';
 import { useNavigation } from '@react-navigation/native';
+import { ALERT_TYPE, Dialog, Root, Toast } from 'react-native-alert-notification';
 
 import { Dimensions } from 'react-native';
 const windowHeight = Dimensions.get('window').height;
@@ -18,42 +20,51 @@ const HomeScreen = () => {
   const navigation = useNavigation();
   let [selectedIndex, setSelectedIndex] = useState(0)
   let userInfo = useSelector(getUserDetails);
+  let { temperature, humidity, vibration } = useSelector(iotData);
   let dispatch = useDispatch();
+
+  if (temperature < 30 || temperature > 40 || humidity < 40 || humidity > 80 || vibration == 1)
+  showNotification(temperature, humidity, vibration)
+
 
   useEffect(() => {
     getDeviceData(dispatch, updateDeviceData)
+
+
   }, [])
-  
+
   return (
-    <View style={styles.mainContainer(windowHeight)}>
-      <View style={styles.topDashboardContainer}>
-        <View style={styles.topRow}>
-          <Text style={styles.topRow_heading}>{`Welcome ${userInfo.name}`}</Text>
-          <TouchableOpacity style={styles.topRow_button} onPress={() => { logOut(navigation) }}>
-            <Text style={styles.topRow_buttonText}>Logout</Text>
-          </TouchableOpacity>
+    <Root>
+      <View style={styles.mainContainer(windowHeight)}>
+        <View style={styles.topDashboardContainer}>
+          <View style={styles.topRow}>
+            <Text style={styles.topRow_heading}>{`Welcome ${userInfo.name}`}</Text>
+            <TouchableOpacity style={styles.topRow_button} onPress={() => { logOut(navigation) }}>
+              <Text style={styles.topRow_buttonText}>Logout</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.screenSelectRow}>
+            <ButtonGroup
+              buttons={['Status', 'Update Number', 'Gps']}
+              selectedIndex={selectedIndex}
+              onPress={(value) => {
+                setSelectedIndex(value);
+              }}
+              containerStyle={{ marginBottom: 0 }}
+              selectedButtonStyle={{ backgroundColor: 'rgb(1, 87, 155)' }}
+            />
+          </View>
         </View>
-        <View style={styles.screenSelectRow}>
-          <ButtonGroup
-            buttons={['Status', 'Update Number', 'Gps']}
-            selectedIndex={selectedIndex}
-            onPress={(value) => {
-              setSelectedIndex(value);
-            }}
-            containerStyle={{ marginBottom: 0 }}
-            selectedButtonStyle={{ backgroundColor: 'rgb(1, 87, 155)' }}
-          />
+
+        <View style={styles.appScreen}>
+
+          {(selectedIndex === 0) ? <StatusScreen /> : (selectedIndex === 1) ? <UpdateNumberScreen /> : <MapScreen />}
+
         </View>
-      </View>
 
-      <View style={styles.appScreen}>
-        
-        {(selectedIndex === 0) ? <StatusScreen /> : (selectedIndex === 1) ? <UpdateNumberScreen /> : <MapScreen /> }
 
       </View>
-
-
-    </View>
+    </Root>
   )
 }
 
@@ -99,3 +110,15 @@ const styles = StyleSheet.create({
   }
 
 })
+
+
+
+function showNotification(temperature, humidity, vibration) {
+  Toast.show({
+    autoClose: 5000,
+    type: ALERT_TYPE.WARNING,
+    title: 'Notification',
+    textBody: `${temperature>40? 'High temperature !\r\n' : temperature<30? 'Low temperature !\r\n' : ''}${humidity>80? 'High humidity !\r\n' : temperature<40? 'Low humidity !\r\n' : ''}${vibration ==1? 'Vibration detected !' : ''}`,
+    onPress: ()=>{  Toast.hide() }
+  })
+}
